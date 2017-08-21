7eaaa1a3cff933851ae280afd645a685c0c1cd02
lex <lex@users.noreply.github.com>
2017-08-21 22:42:06 +0300

    initial commit

---
+++ chip/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chip
+//
+//  Created by lex on 21/08/2017.
+//  Copyright © 2017 lex. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
