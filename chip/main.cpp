c7a82fc73d48ad6f31717812be18b8cf370ace9a
lex <lex@users.noreply.github.com>
2017-08-22 09:45:36 +0300

    add rom loading

--- chip/main.cpp
+++ chip/main.cpp
@@ -7,9 +7,42 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <array>
+
+std::array<uint8_t, 4096> rom{0};
+
+int readRom(const std::string& file) {
+    std::ifstream i(file, std::ios::binary);
+
+    if (!i.is_open()) {
+        std::cout << "couldn't open file " << file << std::endl;
+        return 1;
+    }
+
+    i.read(reinterpret_cast<char*>(&rom), sizeof rom);
+
+    i.close();
+
+    for (const auto& b : rom) {
+        std::cout << b;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    if (argc < 2) {
+        std::cout << "need rom" << std::endl;
+        return 1;
+    }
+
+    const std::string file = argv[1];
+
+    int r = readRom(file);
+
+    return r;
 }
